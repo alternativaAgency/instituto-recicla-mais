ac646b500c04627b1d28bfc87aac86ffde22c1f2
mateusloubach <mcesarloubach@gmail.com>
2025-12-15 00:13:27 -0300

    chore: remove unused component.

--- src/components/sections/Mission.tsx
+++ src/components/sections/Mission.tsx
@@ -159,29 +159,6 @@ const Mission = () => {
   );
 };
 
-const Logo = () => {
-  // Temp logo from https://logoipsum.com/
-  return (
-    <svg
-      width="32"
-      height="auto"
-      viewBox="0 0 50 39"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-fit fill-zinc-950"
-    >
-      <path
-        d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
-        stopColor="#09090B"
-      ></path>
-      <path
-        d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
-        stopColor="#09090B"
-      ></path>
-    </svg>
-  );
-};
-
 const Logos = () => {
   return (
     <div className="relative -mt-2 -rotate-1 scale-[1.01] border-y-2 border-zinc-900 bg-white">
